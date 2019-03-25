986f9fe85ef0ca1a4ede533758f169e692af5cdc
Ingrid Pedersen <ingridklare@gmail.com>
2019-03-24 21:40:58 -0400

    Added header and footer code

--- matches.php
+++ matches.php
@@ -5,15 +5,20 @@
             <meta charset="utf-8" />
             <link href="heart.gif" type="image/gif" rel="shortcut icon" />
             <link href="match.css" type="text/css" rel="stylesheet" />
+            <script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js" type="text/javascript"></script>
     </head>
 	<body>
+		<div id="bannerarea">
+			<img src="match.png" alt="banner logo" /> <br />
+			where meek geeks meet
+		</div>
             <form action="/matches-submit.php" method="get">
                 <fieldset>
                     <legend>Returning User:</legend>
                     <ul class="match">
                         <li class="match">
                             <strong>Name:</strong>
-                            <input type="text" name="matchname" maxlength="16"/>
+                            <input type="text" name="name" maxlength="16"/>
                         </li>
                         <li class="match">
                             <input type="submit" value="View My Matches"/>
@@ -21,5 +26,23 @@
                     </ul>
                 </fieldset>
             </form>
+            <div>
+			<p>
+				This page is for single nerds to meet and date each other!  Type in your personal information and wait for the geek love to begin!  Thank you for using our site.
+			</p>
+
+			<p>
+				Results and page (C) Copyright Match Inc.
+			</p>
+
+			<ul>
+				<li>
+					<a href="index.php">
+						<img src="back.gif" alt="icon" />
+						Back to front page
+					</a>
+				</li>
+			</ul>
+		</div>
 	</body>
 </html>
--- signup.php
+++ signup.php
@@ -5,8 +5,13 @@
             <meta charset="utf-8" />
             <link href="heart.gif" type="image/gif" rel="shortcut icon" />
             <link href="match.css" type="text/css" rel="stylesheet" />
+            <script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js" type="text/javascript"></script>
     </head>
 	<body>
+		<div id="bannerarea">
+			<img src="match.png" alt="banner logo" /> <br />
+			where meek geeks meet
+		</div>
             <form action="/signup-submit.php" method="post">
                 <fieldset>
                     <legend>New User Signup:</legend>
@@ -49,5 +54,23 @@
                     </ul>
                 </fieldset>
             </form>
+            <div>
+			<p>
+				This page is for single nerds to meet and date each other!  Type in your personal information and wait for the geek love to begin!  Thank you for using our site.
+			</p>
+
+			<p>
+				Results and page (C) Copyright Match Inc.
+			</p>
+
+			<ul>
+				<li>
+					<a href="index.php">
+						<img src="back.gif" alt="icon" />
+						Back to front page
+					</a>
+				</li>
+			</ul>
+		</div>
 	</body>
 </html>
