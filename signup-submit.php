f5819ca2abcdab6d0ea7426b6e82863a655073d6
sp3akerb0xx <38464684+sp3akerb0xx@users.noreply.github.com>
2019-03-25 21:33:54 -0400

    Add files via upload

--- signup-submit.php
+++ signup-submit.php
@@ -8,6 +8,35 @@
             <script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.0.0/prototype.js" type="text/javascript"></script>
     </head>
 	<body>
+    <?php
+    $name = $_POST['matchname'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $personality = $_POST['personality'];
+    $operatingSystem = $_POST['operating-system'];
+    $min = $_POST['min'];
+    $max = $_POST['max'];
+
+    if($gender === "male"){
+        $gender = 'M';
+    }
+    else{
+        $gender = "F";
+    }
+
+    if($operatingSystem === "mac"){
+        $operatingSystem = "Mac OS X";
+    }
+    elseif($operatingSystem === "windows"){
+        $operatingSystem = "Windows";
+    }
+    else{
+        $operatingSystem = "Linux";
+    }
+
+    $append_string = $name . ',' . $gender . ',' . $age . ',' . $personality . ',' . $operatingSystem . ',' . $min . ',' . $max; 
+    file_put_contents("singles.txt", $append_string, FILE_APPEND | LOCK_EX);
+    ?>
 		<div id="bannerarea">
 			<img src="match.png" alt="banner logo" /> <br />
 			where meek geeks meet
