04706e887d151587b258b86685117cbd233c6718
sp3akerb0xx <38464684+sp3akerb0xx@users.noreply.github.com>
2019-04-03 11:56:15 -0400

    Update matches-submit.php

--- matches-submit.php
+++ matches-submit.php
@@ -13,12 +13,7 @@
 			where meek geeks meet
 		</div>
 <?php
-error_reporting(0);
-$imageArray;
 $name = $_GET['name'];
-if(empty($name)) {
-    exit("Please enter a name");
-}
 $txtFile = file_get_contents("singles.txt");
 $users = explode("\n", $txtFile);
 $matching_user;
@@ -29,30 +24,65 @@ foreach($users as $user => $data){
         break;
     }
 }
+
 if($matching_user[0] != $name){
-    exit("The entered name is not registered. Please try again...");
+    echo("The entered name is not registered. Please try again...");
 }
 else{
     foreach($users as $user => $data){
         $tempArray = explode(",", $data);
         if(($matching_user[1] != $tempArray[1]) && ($matching_user[2] >= $tempArray[5] && $matching_user[2] <= $tempArray[6]) && ($tempArray[2] >= $matching_user[5] && $tempArray[2] <= $matching_user[6]) && ($matching_user[4] === $tempArray[4])){
+            // $comp1 = explode("", $matching_user[3]);
             $comp1 = str_split($matching_user[3]);
+            // $comp2 = explode("", $tempArray[3]);
             $comp2 = str_split($tempArray[3]);
             for($i = 0; $i < 4; $i++){
                 if($comp1[$i] === $comp2[$i]){
-                    for($j = 0; $j < 7; $j++)    {
-                        echo($imageArray[$j]);
-                    }
+                    // for($j = 0; $j < 7; $j++)    {
+                    //     echo($tempArray[$j]);
+                    // }
+                    $name1=explode(" ", $tempArray[0]);
+                        $image;
+                        if(sizeof($name1)>1){
+                            $image="Images/" . strtolower($name1[0]) . "_" . strtolower($name1[1]) . ".jpg";
+                        }
+                        else{
+                            $image="Images/" . strtolower($name1[0]) . ".jpg";
+                        }
+                        // echo("<ul class=\"match\">");
+                        // echo("<img src=" . $image . ">");
+                        // echo("</ul>");
+                        echo("<ul class=\"match\">
+                        <li class=\"match\">
+                        <img src=" . $image . ">
+                        </li>
+                        <li class=\"match\">
+                        <?php echo(\"$name\" ?>
+                        </li>
+                        <li class=\"match\">
+                            <strong>Name:</strong>
+                        </li>
+                        <li class=\"match\">
+                            <strong>Gender:</strong>
+                        </li>
+                        <li class=\"match\">
+                            <strong>Age:</strong>
+                            
+                        </li>
+                        <li class=\"match\">
+                            <strong>Personality Type:</strong>
+                            
+                        </li>
+                        <li class=\"match\">
+                            <strong>OS:</strong>
+                            </li>
+                        </ul>");
+                    echo("\n");
                     break;
                 }
             }
         }
     }
 }
-
 ?>
-</body>
-    <ul class="match">
-        
-    </ul>
 </html>
