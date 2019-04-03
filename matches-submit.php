a2b935d4084ab9d16e371eb42fde5eb3a95c7149
sp3akerb0xx <38464684+sp3akerb0xx@users.noreply.github.com>
2019-04-03 16:15:45 -0400

    Update matches-submit.php

--- matches-submit.php
+++ matches-submit.php
@@ -36,19 +36,12 @@ if($matching_user[0] != $name){
 else{
     foreach($users as $user => $data){
         $tempArray = explode(",", $data);
-        if(($matching_user[1] != $tempArray[1]) && ($matching_user[2] >= $tempArray[5] && $matching_user[2] <= $tempArray[6]) && ($tempArray[2] >= $matching_user[5] && $tempArray[2] <= $matching_user[6]) && ($matching_user[4] === $tempArray[4])){
-            // $comp1 = explode("", $matching_user[3]);
+        if(($matching_user[1] != $tempArray[1]) && ($matching_user[2] >= $tempArray[5] && $matching_user[2] <= $tempArray[6]) && ($tempArray[2] >= $matching_user[5] && $tempArray[2] <= $matching_user[6]) && ($matching_user[4] === $tempArray[4]))
+        {
             $comp1 = str_split($matching_user[3]);
-            // $comp2 = explode("", $tempArray[3]);
             $comp2 = str_split($tempArray[3]);
             for($i = 0; $i < 4; $i++){
                 if($comp1[$i] === $comp2[$i]){
-                    //for($j = 0; $j < 7; $j++)    {
-                        //echo($imageArray[$j]);
-                    //}
-                    // for($j = 0; $j < 7; $j++)    {
-                    //     echo($tempArray[$j]);
-                    // }
                     $name1=explode(" ", $tempArray[0]);
                         $image;
                         if(sizeof($name1)>1){
@@ -57,28 +50,30 @@ else{
                         else{
                             $image="Images/" . strtolower($name1[0]) . ".jpg";
                         }
-                        // echo("<ul class=\"match\">");
-                        // echo("<img src=" . $image . ">");
-                        // echo("</ul>");
+                        $gender=$tempArray[1];
+                        $personality=$tempArray[3];
+                        $opSystem=$tempArray[4];
+                        $age=$tempArray[2];
                         echo(" <div class= \"match\">
                         <p><img src=" . $image . "> $name1[0] $name1[1]</p>
                         <ul>
                         <li>
-                            <strong>Gender: </strong>
+                            <strong>Gender:  " . $gender . "</strong>
                         </li>
                         <li>
-                            <strong>Age:</strong>
+                            <strong>Age: " . $age . "</strong>
                             
                         </li>
                         <li>
-                            <strong>Personality Type:</strong>
+                            <strong>Personality Type: " . $personality . "</strong>
                             
                         </li>
                         <li>
-                            <strong>OS:</strong>
+                            <strong>OS: " . $opSystem . "</strong>
                             </li>
                         </ul>
                     </div>");
+                    break;
                 }
             }
         }
